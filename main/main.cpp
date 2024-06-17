30fd9123bbce4d448d8d17226f421b7bdc602a66
lo <loouwiit@outlook.com>
2024-06-17 21:34:23 +0800

    修改引脚

--- main/main.cpp
+++ main/main.cpp
@@ -25,7 +25,7 @@ void ioPressed(void* arg);
 void app_main()
 {
 	setIo0();
-	startPWM((gpio_num_t)5);
+	startPWM((gpio_num_t)1);
 	mountFlash();
 
 	ESP_ERROR_CHECK(esp_event_loop_create_default());
