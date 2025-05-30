262bc4c8f344dc712f39cfabca8aa4ca302f82ca
loouwiit <loouwiit@outlook.com>
2025-05-30 12:00:27 +0800

    取消网段限制，对wifiscan提供错误提示

--- main/server/server.cpp
+++ main/server/server.cpp
@@ -137,22 +137,23 @@ void server(void*)
 
 		printf("server: socket accepted ip address: %s\n", addr_str);
 
-		if (!(addr_str[0] == '1'
-			&& addr_str[1] == '9'
-			&& addr_str[2] == '2'
-			&& addr_str[3] == '.'
-			&& addr_str[4] == '1'
-			&& addr_str[5] == '6'
-			&& addr_str[6] == '8'
-			&& addr_str[7] == '.'
-			&& addr_str[8] == '4'
-			&& addr_str[9] == '.'))
-		{
-			ISocketStream t{ sock };
-			t.close();
-			ESP_LOGI(TAG, "abandoned");
-			continue;
-		}
+		// 对访问网段进行限制
+		// if (!(addr_str[0] == '1'
+		// 	&& addr_str[1] == '9'
+		// 	&& addr_str[2] == '2'
+		// 	&& addr_str[3] == '.'
+		// 	&& addr_str[4] == '1'
+		// 	&& addr_str[5] == '6'
+		// 	&& addr_str[6] == '8'
+		// 	&& addr_str[7] == '.'
+		// 	&& addr_str[8] == '4'
+		// 	&& addr_str[9] == '.'))
+		// {
+		// 	ISocketStream t{ sock };
+		// 	t.close();
+		// 	ESP_LOGI(TAG, "abandoned");
+		// 	continue;
+		// }
 
 		size_t index = 0;
 		while (!socketStreamWindows[index].setSocket(sock))
@@ -455,6 +456,16 @@ void httpPost(IOSocketStream& socketStream, HttpRequest& request)
 	}
 	else if (stringCompare((char*)uri, uriLenght, "/api/wifiScan", 13))
 	{
+		if (!wifiStationIsStarted())
+		{
+			// 发送错误
+			HttpRespond respond;
+			respond.setBody((void*)"ERROR:wifi not started");
+			respond.setBodyLenght(22);
+			respond.send(socketStream);
+			return;
+		}
+
 		// 扫描
 		wifi_ap_record_t* wifi = new wifi_ap_record_t[wifiApRecordSize]; // 96 bytes per record
 		size_t count = wifiStationScan(wifi, wifiApRecordSize);
